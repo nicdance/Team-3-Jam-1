f329a17fd2157fa0ab1e7e60c56eb3159cb241ae
Nicole <nicoledance@chaosshard.com.au>
2020-08-12 16:16:16 +1000

    Minor script changes and removed extra capsule from player

--- Team 3 Platformer/Assets/Script/PlayerController.cs
+++ Team 3 Platformer/Assets/Script/PlayerController.cs
@@ -1,10 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using System.Transactions;
-using TreeEditor;
 using UnityEngine;
-using Vector3 = UnityEngine.Vector3;
 
 public class PlayerController : MonoBehaviour
 {
