5a2e58529fd7d137acaca57b11f5870a9d93bbf4
Nicole <nicoledance@chaosshard.com.au>
2020-08-30 16:27:12 +1000

    Set up action on collision  script to enable actions based on player interacting with bounce objects.

---
+++ Team 3 Platformer/Assets/ActionOnCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionOnCollision : MonoBehaviour
+{
+    [SerializeField]
+    protected float modifier=10f;
+    [SerializeField]
+    protected CollisionAction action;
+    public enum CollisionAction
+    { 
+        BOUNCY,
+        STEAMPOWERED                    
+    }
+
+    public void PerformAction(NicolePlayerController player) {
+        player.rb.AddForce(Vector3.up * modifier, ForceMode.Impulse);
+    }
+}
